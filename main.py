2ad282f11c23b4ab2985f8d53bf87be0c1b29de9
Anthony Lay <antho.lay1994@protonmail.com>
2025-07-15 17:54:00 +1000

    Starting to figure out Git

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ def add_record():
 def is_valid_date():
 
     date_input = input(print("What is the date Format (DD/MM/YYYY): "))
-    day_string, month_string, year_string =
+    day_string, month_string, year_string = date_input.split('/')
 
+
+
+
+def daysInMonth(month, year,):
+
+    #Leap Year
+    if (year % 400 == 0 & year % 4 == 0):
+        print("This is a leap year, There are 29 days in Feb")
+        if (month == 2):
+            print("There are 29 days in this month?")
