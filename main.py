05a4bf067925b80c9b7f1fb4da466dafbdc660e0
Anthony Lay <antho.lay1994@protonmail.com>
2025-07-15 17:48:07 +1000

    Starting to figure out Git

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 appointment_lists = []
 
-def add_appointment():
+def add_record():
 
 
 
 
 
 
+def is_valid_date():
 
+    date_input = input(print("What is the date Format (DD/MM/YYYY): "))
+    day_string, month_string, year_string =
 
