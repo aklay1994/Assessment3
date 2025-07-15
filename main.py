79f98e658520d3db4d25f205a08c88c793683eb7
Anthony Lay <antho.lay1994@protonmail.com>
2025-07-15 18:57:22 +1000

    Starting to figure out Git

--- main.py
+++ main.py
@@ -18,52 +18,27 @@ def is_valid_date():
     month = int(month_string)
     day = int(day_string)
 
-    if (2000 < year < 3000):
-        return year
+    if (1<= day <= daysInMonth(month, year)):
+        print("This is a valid Day")
     else:
-        print("This is a false year.")
+        print("This is not a valid day")
 
 
-    if (1 <= month <= 12):
-        return month
-    else:
-        print("This is a false month")
-
-
-    daysinmonth = int(daysInMonth(month, year))
-
-    if (1 <= day <= daysinmonth):
-        return day
-
-    else:
-        print("This is a false day")
-
-    print("year: " + year)
-    print("month:" + month)
-    print("day:" + day)
 
 
 
 
 def daysInMonth(month, year,):
 
-    #Leap Year
-    if (year % 400 == 0 & year % 4 == 0 & year % 100 != 100):
-        print("This is a leap year, There are 29 days in Feb")
-        if (month == 2):
+    if month in [1,3,5,7,8,10,12]:
+        return 31
+    elif month in [4,6,9,11]:
+        return 30
+    elif month == 2:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
             return 29
         else:
-           return print("Only month that changes is feb.")
-
-
-    else:
-        if (month == 1, 3,5, 7, 8, 10 ,12):
-            print("There are 31 days in this month")
-            return 31
-        elif(month == 4,6,9,11):
-            print("There are 30 days in this month")
-            return 30
-
+            return 28
 
 
 
